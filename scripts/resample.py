53d1058d7a68cf93a2fbf155d8a3a23226e47b0c
zhuozhao <zhuozhao@my.uri.edu>
2019-11-18 15:17:57 -0500

    create a folder to store scripts

---
+++ scripts/resample.py
@@ -0,0 +1,64 @@
+#%%
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from imblearn.over_sampling import SMOTE
+
+
+datapath = "~/cecl_dnn/data/withlabel.csv"
+df = pd.read_csv(datapath)
+
+# Delete loans that are default and exceeds time_to_d
+df = df[~((df["time_to_d"]<0) & (df["d_flag"]==1))]
+
+# Set all good loans time_to_d as 99
+def set_g(row):
+    if row["d_flag"] == 0:
+        return 99
+    else:
+        return row["time_to_d"]
+    
+df["time_to_d"] = df.apply(set_g, axis=1)
+
+train_set, test_set = train_test_split(df, test_size = 0.2)
+test_set.to_csv("test.csv")
+#%%
+# Vectorization for Categorical data
+# Categorial Fields to be encoded using onehot method
+loans_cat_1hot = ["flag_fthb", "ppmt_pnlty", "st"]
+
+pipline_1hot = ColumnTransformer([
+    ("1hot", OneHotEncoder(), loans_cat_1hot)
+])
+
+# Normalization for numerical data
+loans_num_norm = ["orig_loan_term","loan_age","fico","mi_pct",
+                  "cltv","dti","ltv","int_rt","current_upb",
+                  "current_int_rt"]
+
+pipline_norm = ColumnTransformer([
+    ("num", Normalizer(), loans_num_norm)
+])
+
+#%%
+# Resample the data
+pipeline = ColumnTransformer([
+    ("num",Normalizer(),loans_num_norm),
+    ("cat",OneHotEncoder(), loans_cat_1hot)
+])
+
+X_train = pipeline.fit_transform(train_set)
+smtenn = SMOTE(random_state=42, k_neighbors=3)
+res_train, res_target = smtenn.fit_sample(X_train, train_set["time_to_d"])
+
+res_target = res_target.reshape(-1,1)
+res_target = OneHotEncoder(categories="auto").fit_transform(y).toarray()
+# %%
+# save resampled data
+np.save("res_train.npy", res_train.toarray())
+np.save("res_target.npy", res_target)
+# %%
